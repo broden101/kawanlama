ef23c2cefb71511013f9211be2b518d6d83c56ed
broden101 <baguspolikarpus@gmail.com>
2024-11-25 05:04:19 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -71,6 +71,10 @@ export function saveTransaction(transaction) {
     return push(historyRef, transaction);
 }
 
+export function deleteTransaction(transactionId) {
+    return db.ref(`transactions/${transactionId}`).remove(); // Hapus transaksi berdasarkan ID
+}
+
 export function listenToHistory(callback) {
     onValue(historyRef, (snapshot) => {
         const data = snapshot.val();
